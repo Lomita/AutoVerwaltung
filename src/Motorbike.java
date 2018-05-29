03bec3d2d8775b61db357ad949ed629901f1decd
Samuel Gfeller <samuelgfeller@bluewin.ch>
2018-05-29 11:31:44 +0200

    extends

--- src/Motorbike.java
+++ src/Motorbike.java
@@ -3,13 +3,25 @@
   * @author Miruku
   * contains the motorbike definition
   */
-public class Motorbike 
+public class Motorbike extends Vehicle
 {
 	/**
 	 * Constructor
 	 */
 	public Motorbike()
 	{
-		
+	
+	
+	}
+	
+	/**
+	 *  calculates the effective price 
+	 * @return returns the effective price 
+	 */
+	protected float getCalculatedPrice()
+	{	
+		float price;
+		price = this.getOriginalPrice();
+		return price;
 	}
 }
