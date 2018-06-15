b9b45831fdb33bf7f4b990aa96dbfc4bca0f44d3
Samuel Gfeller <samuelgfeller@bluewin.ch>
2018-06-15 08:53:45 +0200

    dunno

--- src/Business/Garage.java
+++ src/Business/Garage.java
@@ -9,11 +9,19 @@ import Data.VehicleList;
 public class Garage {
 	VehicleDAO list;
 
+	public Garage() {
+		VehicleDAO list = new VehicleList();
+	}
+	
 	public void addVehicle(Vehicle vehicle){
 		list.addVehicle(vehicle);
 	}
+	
+	public ArrayList<Vehicle> getVehicleList(){
+		return list.getVehicleList();
+	}
 
-	public Garage() {
-		VehicleDAO list = new VehicleList();
+	public void delVehicle(Vehicle vehicle) {
+		list.removeVehicle(vehicle);
 	}
 }
