572319c3dace86d2da78b9fa4a7a865319c65ef1
Miruku <archangel0660@gmail.com>
2018-06-26 11:25:45 +0200

    Tests

---
+++ src/Tests/TestBusiness.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import Business.*;
+import DTO.Vehicle;
+
+public class TestBusiness 
+{
+	Garage gar;
+	int index;
+	
+	@Before
+	public void init()
+	{
+		gar = new Garage();
+		index = 0;
+	}
+	
+	@Test
+	public void testAddVehicle() 
+	{
+		Vehicle vehicle = new Car("Car","Honda","Auto1",2000,300,20,10000,"Blau",4); 
+		gar.addVehicle(vehicle);
+		assertEquals(vehicle, gar.getVehicleObjFromListByID(index));
+	}
+	
+	@Test
+	public void testCalculatedPrice() 
+	{
+		Vehicle vehicle = new Car("Car","Honda","Auto1",2000,300,20,30000000,"Blau",4); 
+		gar.addVehicle(vehicle);
+		int price = 0;
+		assertTrue(price == gar.getVehicleObjFromListByID(index).getCalculatedPrice());
+	}
+	
+	@After
+	public void dispose()
+	{
+		gar = null;
+	}
+}
